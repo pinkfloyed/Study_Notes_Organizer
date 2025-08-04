c9383c94e32dd066fe69e35e1e1009359dedfe48
pinkfloyed <pinkiakter195@gmail.com>
2025-08-04 20:46:05 +0600

    chore: add global toast styling wrapper component
    - Placeholder component for consistent toast styles
    
    - Actual toast triggered dynamically via JavaScript

---
+++ frontend/src/components/ToastNotification.vue
@@ -0,0 +1,7 @@
+<script setup>
+// This is just a global wrapper for styling if you want custom look
+</script>
+
+<template>
+  <!-- This component is only for reference, actual toast is triggered via JS -->
+</template>
